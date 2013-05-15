28779464065a4d305b7a083da6f5ccd6d77121cc
rsubramanian <Ramanathan.TirunellaiSubramanian@careerbuilder.com>
2013-05-15 13:59:53 -0400

    5152013_2

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -5,20 +5,20 @@ class InvoiceController < ApplicationController
   helper_method :sort_column, :sort_direction
 
   def index
-       @settings = Account.get_saml_settings
-
+    #   @settings = Account.get_saml_settings
     #
-    if @settings.nil?
-      return redirect_to '/saml'
-    end
-
-    # If we're viewing this unauthenticated, set our goback URL for after logging in
-    if session[:userid].nil?
-      #session[:goback_to] = request.url
-      session[:userid]=nil
-      return redirect_to '/saml'
-
-    end
+    ##
+    #if @settings.nil?
+    #  return redirect_to '/saml'
+    #end
+    #
+    ## If we're viewing this unauthenticated, set our goback URL for after logging in
+    #if session[:userid].nil?
+    #  #session[:goback_to] = request.url
+    #  session[:userid]=nil
+    #  return redirect_to '/saml'
+    #
+    #end
 
     respond_to do |format|
       format.html # index.html.erb
