4983118f641668264de36834be0d0ea788766840
rsubramanian <Ramanathan.TirunellaiSubramanian@careerbuilder.com>
2013-05-15 15:27:28 -0400

    5152013_4

--- app/models/invoice_manager.rb
+++ app/models/invoice_manager.rb
@@ -8,6 +8,8 @@ class InvoiceManager
 
   def get_invoice_by_accountdid(accountdid,status)
 
+    puts @web_svc_url
+
     options = {}
     if status == 'Open'
        invStatus = 'OPN'
@@ -17,5 +19,10 @@ class InvoiceManager
       invStatus = 'ALL'
     end
     res = self.class.get(@web_svc_url+'/api/invoices?AccountDID='+accountdid+'&Status='+invStatus, options)
+
+    puts res
+
+    res
+
   end
 end
