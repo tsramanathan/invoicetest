2fd8dc8a2dd8d372d73e6c8a3d56687fd7d3b3b2
rsubramanian <Ramanathan.TirunellaiSubramanian@careerbuilder.com>
2013-05-16 14:06:49 -0400

    5162013

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,8 +7,8 @@ class Account < ActiveRecord::Base
     # should retrieve SAML-settings based on subdomain, IP-address, NameID or similar
     settings = Onelogin::Saml::Settings.new
 
-    settings.assertion_consumer_service_url   = "http://localhost:3000/saml/consume"
-    settings.issuer                           = "http://localhost:3000" # the name of your application
+    settings.assertion_consumer_service_url   = "http://ec2-54-225-110-64.compute-1.amazonaws.com/saml/consume"
+    settings.issuer                           = "http://ec2-54-225-110-64.compute-1.amazonaws.com/" # the name of your application
     settings.idp_sso_target_url               = "https://cbtest.okta.com/app/template_saml_2_0/k4aberjrODWHGWIYFSXI/sso/saml"
     #settings.idp_slo_target_url = "https://cbtest.okta.com/home/template_saml_2_0/0oa4aberjsHPFYSRBJOX/3079"
     settings.idp_cert_fingerprint             = "02:05:79:D8:96:CC:CA:54:2F:16:E2:6E:7D:32:F3:54:95:61:89:71"
